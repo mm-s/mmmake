af305ce032b805a60d11e9500cbe99fc16961e47
Marcos Mayorga <marcos@ncrypt.com>
2017-04-12 10:33:58 +0100

    using cmake's openssl

--- share/mmmake/templates/link_ssl.cmake
+++ share/mmmake/templates/link_ssl.cmake
@@ -1,5 +1,9 @@
-include_directories(${SSL_INCLUDE})
+#include_directories(${SSL_INCLUDE})
+include_directories(${OPENSSL_INCLUDE_DIR})
 
-FOREACH(LINKLIB ${SSL_LIBS})
+
+#FOREACH(LINKLIB ${SSL_LIBS})
+FOREACH(LINKLIB ${OPENSSL_LIBRARIES})
+
 target_link_libraries(${prjname} ${LINKLIB})
 ENDFOREACH(LINKLIB)
